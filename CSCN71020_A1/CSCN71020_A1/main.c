e8eb1ca3c81f69ca2e4f9d70f42162d1db71cb90
Ivan Immanuel <ivanimmanuel1234@gmail.com>
2026-02-13 16:35:35 -0500

    Subtract function re-written and main() updated.

--- CSCN71020_A1/CSCN71020_A1/main.c
+++ CSCN71020_A1/CSCN71020_A1/main.c
@@ -3,6 +3,7 @@
 void printWelcomeMenu();
 void printOptions();
 void add();
+void subtract();
 
 void main() 
 {
@@ -15,7 +16,13 @@ void main()
 	{
 		case 1:
 			add();
+		case 2:
+			subtract();
+			break;
+		default:
+			printf("Invalid input.\n");
 	}
+	return 0;
 }
 
 void printWelcomeMenu() 
@@ -41,4 +48,15 @@ void add()
 	scanf_s("%lf", &num2);
 	result = num1 + num2;
 	printf("%lf + %lf = %lf\n", num1, num2, result);
+}
+
+void subtract()
+{
+	double num1, num2, result;
+	printf("Enter the first value:");
+	scanf_s("%lf", &num1);
+	printf("Enter the second value:");
+	scanf_s("%lf", &num2);
+	result = num1 - num2;
+	printf("%lf - %lf = %lf\n", num1, num2, result);
 }
